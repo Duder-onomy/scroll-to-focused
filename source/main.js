6c35a5347f11ca6776b540a9b3331c7b737f54ba
Greg Larrenaga <greg.larrenaga@thinksolid.com>
2015-03-19 17:15:05 -0700

    Closes #3 Removing the reserved word Private.

--- source/main.js
+++ source/main.js
@@ -6,7 +6,7 @@ define(['lodash', 'velocity'], function (_, Velocity) {
         configure : configure,
         start : start,
 
-        private : {
+        internal : {
             config : {
                 container : null,
                 focusedSelector : '.focused',
@@ -26,52 +26,52 @@ define(['lodash', 'velocity'], function (_, Velocity) {
     };
 
     function configure(options) {
-        _.merge(this.private.config, options);
+        _.merge(this.internal.config, options);
 
         return this;
     }
 
     function start() {
-        if(!this.private.config.container) {
+        if(!this.internal.config.container) {
             console.log('You initialized scrollToFocused with no container.');
         }
-        if(!this.private.config.container.nodeName) {
-            this.private.config.container = document.querySelector(this.private.config.container);
+        if(!this.internal.config.container.nodeName) {
+            this.internal.config.container = document.querySelector(this.internal.config.container);
         }
 
-        this.private.windowDimensions = this.private.config.container.getBoundingClientRect();
+        this.internal.windowDimensions = this.internal.config.container.getBoundingClientRect();
 
         _startWatchingContainerForScrollEvents.call(this);
     }
 
     function _startWatchingContainerForScrollEvents() {
-        this.private.config.container.addEventListener('focus-gained', _.throttle(_scrollToFocusedElement.bind(this), this.private.config.throttle));
+        this.internal.config.container.addEventListener('focus-gained', _.throttle(_scrollToFocusedElement.bind(this), this.internal.config.throttle));
     }
 
     function _scrollToFocusedElement() {
-        var focusedElement = this.private.config.container.querySelector(this.private.config.focusedSelector),
+        var focusedElement = this.internal.config.container.querySelector(this.internal.config.focusedSelector),
             focusedElementsBoundingRect = focusedElement.getBoundingClientRect();
 
-        if(focusedElementsBoundingRect.left < this.private.windowDimensions.left) { // focus is off the left side of window
-            if(this.private.config.animate) {
+        if(focusedElementsBoundingRect.left < this.internal.windowDimensions.left) { // focus is off the left side of window
+            if(this.internal.config.animate) {
                 Velocity(focusedElement, 'scroll', {
-                    container : this.private.config.container,
+                    container : this.internal.config.container,
                     axis : 'x',
                     offset : focusedElementsBoundingRect.width / 2 * -1
                 });
             } else {
-                this.private.config.container.scrollLeft = this.private.config.container.scrollLeft - Math.abs(this.private.windowDimensions.left - focusedElementsBoundingRect.left) - focusedElementsBoundingRect.width / 2;
+                this.internal.config.container.scrollLeft = this.internal.config.container.scrollLeft - Math.abs(this.internal.windowDimensions.left - focusedElementsBoundingRect.left) - focusedElementsBoundingRect.width / 2;
             }
         }
-        if(focusedElementsBoundingRect.right > this.private.windowDimensions.right) { // focus is off the right side of window
-            if(this.private.config.animate) {
+        if(focusedElementsBoundingRect.right > this.internal.windowDimensions.right) { // focus is off the right side of window
+            if(this.internal.config.animate) {
                 Velocity(focusedElement, 'scroll', {
-                    container : this.private.config.container,
+                    container : this.internal.config.container,
                     axis : 'x',
-                    offset : (this.private.windowDimensions.width - focusedElementsBoundingRect.width * 1.5) * -1
+                    offset : (this.internal.windowDimensions.width - focusedElementsBoundingRect.width * 1.5) * -1
                 });
             } else {
-                this.private.config.container.scrollLeft = this.private.config.container.scrollLeft + Math.abs(this.private.windowDimensions.width - focusedElementsBoundingRect.right) + focusedElementsBoundingRect.width / 2;
+                this.internal.config.container.scrollLeft = this.internal.config.container.scrollLeft + Math.abs(this.internal.windowDimensions.width - focusedElementsBoundingRect.right) + focusedElementsBoundingRect.width / 2;
             }
         }
     }
